3b67c283a63d51dfa27d6602f97ea694b2115b8a
Aminu Rabiu <arabiu033@gmail.com>
2022-11-08 15:48:11 +0100

    Add redux hooks to the module

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { status } from '../redux/categories/categories';
 
-const Categories = () => (
-  <button type="button">Check Status</button>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  return (
+    <button type="button" onClick={() => dispatch(status)}>Check Status</button>
+  );
+};
 
 export default Categories;
