de34d55d2a98578fbc104850d45a6c432083cab4
Aminu Rabiu <arabiu033@gmail.com>
2022-11-08 16:51:20 +0100

    Update changes

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { status } from '../redux/categories/categories';
 
 const Categories = () => {
+  const text = useSelector((state) => state.categories);
   const dispatch = useDispatch();
   return (
-    <button type="button" onClick={() => dispatch(status)}>Check Status</button>
+    <>
+      <h2>{text}</h2>
+      <button type="button" onClick={() => dispatch(status())}>Check Status</button>
+    </>
   );
 };
 
