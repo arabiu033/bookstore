bcdf845436e89e40b7e4cfb9bb336023708348b7
Aminu Rabiu <arabiu033@gmail.com>
2022-11-11 09:17:17 +0100

    Add some default list of books

--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -1,15 +1,26 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
 import api from '../../api/bookstoreApi';
 
 const ADD = 'bookstore/books/ADD';
 const REMOVE = 'bookstore/books/REMOVE';
 const FETCH = 'bookstore/books/FETCH';
 
-const initialState = [];
+const initialState = [
+  {
+    item_id: uuidv4(), title: 'The Hunger Games', author: 'Suzanne Collins',
+  },
+  {
+    item_id: uuidv4(), title: 'Dune', author: 'Frank Herbert',
+  },
+  {
+    item_id: uuidv4(), title: 'Capital in the Twenty-First Century', author: 'Suzanne Collins',
+  },
+];
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case FETCH: return [...action.payload];
+    case FETCH: return [...state, ...action.payload];
     case ADD: return [...state, action.payload];
     case REMOVE: return state.filter((f) => f.item_id !== action.payload);
     default: return state;
