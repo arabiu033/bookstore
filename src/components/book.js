aa54afba2b6e58b7b7ae3a0289437eb64ca9b5ba
Aminu Rabiu <arabiu033@gmail.com>
2022-11-11 09:30:04 +0100

    Refactor the component to display some dummy info

--- src/components/book.js
+++ src/components/book.js
@@ -2,23 +2,45 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { remove } from '../redux/books/book';
+import styles from '../css/book.module.css';
 
 const Book = (props) => {
-  const { book } = props;
+  const { book, inputs } = props;
   const dispatch = useDispatch();
-  const style = { display: 'flex', gap: '1rem', alignItems: 'baseline' };
 
   return (
-    <li style={style}>
-      <div style={style}>
-        <h3>{book.title}</h3>
-        <p>{book.author}</p>
-      </div>
-      <button type="button" onClick={() => dispatch(remove(book.item_id))}>Remove</button>
+    <li className={styles.container}>
+      <section>
+        <span className={styles.categories}>{inputs.categories}</span>
+        <h3 className={styles.title}>{book.title}</h3>
+        <p className={styles.author}>{book.author}</p>
+        <div className={styles.actions}>
+          <button type="button">Comments</button>
+          <button type="button" onClick={() => dispatch(remove(book.item_id))}>Remove</button>
+          <button type="button">Edit</button>
+        </div>
+      </section>
+
+      <section className={styles.progress}>
+        <div className={styles.circle} />
+        <div className={styles.pr1}>
+          <span className={styles.pN}>{inputs.percentage}</span>
+          <span className={styles.pC}>Completed</span>
+        </div>
+      </section>
+
+      <section>
+        <span>CURRENT CHAPTER</span>
+        <p>{inputs.ch}</p>
+        <button type="button">UPDATE PROGRESS</button>
+      </section>
     </li>
   );
 };
 
-Book.propTypes = { book: PropTypes.objectOf(PropTypes.string).isRequired };
+Book.propTypes = {
+  book: PropTypes.objectOf(PropTypes.string).isRequired,
+  inputs: PropTypes.objectOf(PropTypes.string).isRequired,
+};
 
 export default Book;
